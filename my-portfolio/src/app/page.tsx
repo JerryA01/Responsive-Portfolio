1fa1ea791a3f349951e83090b8614958e11b1360
Jerry Dev <jerryarthur075@gmail.com>
2025-07-24 08:59:53 +0100

    feat: finalized responsive layout for smartphones

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -4,18 +4,18 @@ import Image from "next/image";
 import { FaGithub, FaArrowRight } from "react-icons/fa";
 
 export default function Home() {
-  const showBorders = false;
+  const showBorders = true;
 
   return (
     <div
-      className={`grid grid-cols-3 gap-y-0 gap-x-0 p-8 ${showBorders ? "border border-white-400" : ""
+      className={`grid grid-cols-1 2xl:grid-cols-3 gap-y-0 gap-x-0 p-8 ${showBorders ? "border border-white-400" : ""
         }`}
       style={showBorders ? { outline: "1px solid red" } : {}}
     >
       {/* === Top Row (Full Width - Image Right, Text Center) === */}
-      <div className={`${showBorders ? "border border-blue-500" : ""} col-span-3 flex flex-row items-start`}>
+      <div className={`${showBorders ? "border border-blue-500" : ""} col-span-1 2xl:col-span-3 flex flex-col 2xl:flex-row items-center 2xl:items-start`}>
         {/* Text */}
-        <div className="w-2/3 text-center translate-x-72">
+        <div className="2xl:w-2/3 text-center 2xl:translate-x-72 ">
           <div className="font-raleway text-7xl text-white drop-shadow-md font-light mb-2 mt-4">
             Jerry Arthur
           </div>
@@ -28,7 +28,7 @@ export default function Home() {
         </div>
 
         {/* Image */}
-        <div className="flex justify-end w-1/3 -translate-x-45">
+        <div className="flex justify-center w-1/3 2xl:-translate-x-45">
           <div className="relative w-[160px] h-[160px] rounded-full bg-gradient-to-r from-pink-500 via-yellow-500 to-purple-500 animate-spinSlow p-[5px]">
             <div className="rounded-full bg-white w-full h-full flex items-center justify-center">
               <Image
@@ -50,7 +50,7 @@ export default function Home() {
         {/* Decorative Horizontal Line */}
         <div className="w-2/3 h-[2px] bg-black opacity-80 mx-auto mt-12 mb-2 rounded-full py-4">
           {/* Hanging Squiggly Lines */}
-          <div className="relative w-3/3 mx-auto grid grid-cols-3">
+          <div className="hidden 2xl:grid relative w-3/3 mx-auto 2xl:grid-cols-3">
 
             {/* Left Squiggle */}
             <svg className="justify-self-start ml-85" width="24" height="249" viewBox="0 0 24 290 ">
@@ -100,34 +100,32 @@ export default function Home() {
       {/* Row between the bottom and middle row */}
       {/* === Section Title Divider === */}
       <div className={`${showBorders ? "border border-blue-500" : ""} col-span-3 `}>
-        <h2 className="sway text-center text-4xl font-bold text-blue-700 font-raleway">
+        <h2 className="sway text-center text-3xl sm:text-4xl md:text-4xl 2xl:text-4xl font-bold text-blue-700 font-raleway">
           My Projects
         </h2>
       </div>
 
 
       {/* === Second to Bottom Row () === */}
-      <div className={`${showBorders ? "border border-blue-500" : ""} col-span-3 mt-27`}>
+      <div className={`${showBorders ? "border border-blue-500" : ""} col-span-1 2xl:col-span-3 2xl:mt-27`}>
 
-        <div className="sway flex justify-between items-center mb-1">
-
+        <div className={`sway flex flex-col 2xl:flex-row 2xl:justify-between items-center 2xl:mb-1 mt-1 ${showBorders ? "border border-red-500" : ""}`}>
           {/* Left Title */}
-          <div className="font-raleway text-3xl text-blue-700 font-semibold text-center ml-126">
+          <div className="font-raleway text-2xl sm:text-3xl 2xl:text-3xl text-blue-700 font-semibold text-center 2xl:ml-126">
             Social Media Application
           </div>
 
           {/* Right Title */}
-          <div className="font-raleway text-3xl text-blue-700 font-semibold mr-113">
+          <div className="hidden 2xl:block font-raleway text-2xl 2xl:text-3xl text-blue-700 font-semibold 2xl:mr-113">
             Tic Tac Toe Game
           </div>
-
         </div>
 
         {/* Video + Text Row */}
-        <div className="w-full flex flex-row justify-start items-start">
+        <div className="w-full flex flex-col items-center 2xl:flex-row 2xl:justify-start 2xl:items-start ">
 
           {/* Description Box Left*/}
-          <div className="sway w-[300px] h-[350px] p-4 bg-blue-800 text-white rounded-full font-raleway text-center shadow-lg mt-1 ml-20">
+          <div className="sway 2xl:w-[300px] 2xl:h-[350px] w-[300px] h-[350px] sm:w-[330px] sm:h-[370px] md:w-[390px] md:h-[410px] p-4 bg-blue-800 text-white rounded-full font-raleway text-center shadow-2xl mt-5 2xl:mt-1 2xl:ml-20">
             <h3 className="text-xl font-semibold mb-2 text-blue-200 mt-7 ">What This App Does</h3>
             <p>
               Chirrup is a social media app for university students with real-time posts, media sharing, and threaded comments.
@@ -151,8 +149,8 @@ export default function Home() {
           </div>
 
           {/* Video Frame - Left */}
-          <div className="sway p-3 rounded-lg bg-gradient-to-r from-blue-600 via-blue-500 to-blue-600 animate-spinSlow mt-4 z-10">
-            <div className="bg-white rounded-md overflow-hidden w-[550px] aspect-video">
+          <div className="sway p-3 rounded-2xl bg-gradient-to-r from-blue-600 via-blue-500 to-blue-600 animate-spinSlow mt-4 z-10">
+            <div className="bg-white rounded-md overflow-hidden 2xl:w-[550px] md:w-[610px] aspect-video">
               <iframe
                 className="w-full h-full"
                 src="https://www.youtube.com/embed/e2Op1XRsc7Q"
@@ -164,9 +162,16 @@ export default function Home() {
             </div>
           </div>
 
+          {/* Mobile-only centered title */}
+          <div className="2xl:hidden sway w-full flex justify-center mt-4 ">
+            <div className="font-raleway text-2xl sm:text-3xl md:text-4xl text-blue-700 font-semibold text-center">
+              Tic Tac Toe Game
+            </div>
+          </div>
+
           {/* Video Frame - Right */}
-          <div className="sway p-3 rounded-lg bg-gradient-to-r from-blue-600 via-blue-500 to-blue-600 animate-spinSlow mt-4 ml-7">
-            <div className="bg-white rounded-md overflow-hidden w-[550px] aspect-video">
+          <div className="sway p-3 rounded-2xl bg-gradient-to-r from-blue-600 via-blue-500 to-blue-600 animate-spinSlow mt-4 2xl:ml-7">
+            <div className="bg-white rounded-md overflow-hidden 2xl:w-[550px] md:w-[610px] aspect-video">
               <iframe
                 className="w-full h-full"
                 src="https://www.youtube.com/embed/Nq2k1KxFM3s"
@@ -179,7 +184,7 @@ export default function Home() {
           </div>
 
           {/* Description Box - Right */}
-          <div className="sway w-[300px] h-[350px] p-4 bg-blue-800 text-white rounded-full font-raleway text-center shadow-lg mt-1">
+          <div className="sway w-[300px] h-[350px] sm:w-[330px] sm:h-[370px] md:w-[390px] md:h-[410px] 2xl:h-[350px] 2xl:w-[300px] p-4 bg-blue-800 text-white rounded-full font-raleway text-center shadow-2xl mt-6 2xl:mt-1">
             <h3 className="text-xl font-semibold mb-2 text-blue-200 mt-7">What This Game Does</h3>
             <p>
               Multiplayer Tic Tac Toe game built in Java with real-time play, GUI controls, and synchronized server-client communication.
@@ -208,15 +213,15 @@ export default function Home() {
 
 
       {/* === Bottom Row () === */}
-      <div className={`${showBorders ? "border border-blue-500" : ""} col-span-3 -mt-5 `}>
+      <div className={`${showBorders ? "border border-blue-500" : ""} col-span-3 -mt-7`}>
 
         {/* Hanging Squigglies Between Middle & Bottom Rows */}
-        <div className="w-2/3 mx-auto rounded-full">
+         <div className="w-2/3 mx-auto rounded-full">
           <div className="relative w-full grid grid-cols-3">
 
             {/* Left Squiggle */}
-            <svg className="justify-self-start ml-85 z-0" width="24" height="249" viewBox="0 0 24 290" >
-              <path stroke="black" strokeWidth="2.5" fill="none">
+            <svg className="stroke-0 2xl:stroke-[2.5] justify-self-start ml-85 z-0" width="24" height="249" viewBox="0 0 24 290" >
+              <path stroke="black" fill="none">
                 <animate
                   attributeName="d"
                   dur="1.2s"
@@ -230,12 +235,12 @@ export default function Home() {
               </path>
             </svg>
           </div>
-        </div>
+         </div>
 
         {/* Video + Text Row */}
-        <div className="w-full flex flex-row justify-start items-start -mt-5 ">
+        <div className="w-full flex flex-col 2xl:flex-row items-center justify-start 2xl:-mt-9">
           {/* Description Box Left*/}
-          <div className="sway w-[300px] h-[350px] p-4 bg-blue-800 text-white rounded-full font-raleway text-center shadow-lg  ml-20">
+          <div className="sway w-[300px] h-[350px] sm:w-[330px] sm:h-[370px] md:w-[390px] md:h-[410px] 2xl:h-[350px] 2xl:w-[300px] p-4 bg-blue-800 text-white rounded-full font-raleway text-center shadow-2xl -mt-52 2xl:mt-2 2xl:ml-20">
             <h3 className="text-xl font-semibold mb-2 text-blue-200 mt-7 ">Coming soon...</h3>
             <p>
               ...
@@ -254,8 +259,8 @@ export default function Home() {
             </p>
           </div>
           {/* Video Frame - Left */}
-          <div className="sway p-3 rounded-lg bg-gradient-to-r from-blue-600 via-blue-500 to-blue-600 animate-spinSlow ">
-            <div className="bg-white rounded-md overflow-hidden w-[550px] aspect-video">
+          <div className="sway p-3 rounded-2xl bg-gradient-to-r from-blue-600 mt-6 2xl:-mt-1 via-blue-500 to-blue-600 animate-spinSlow ">
+            <div className="bg-white rounded-md overflow-hidden w-[283px] md:w-[610px] 2xl:w-[520px] aspect-video">
               <iframe
                 className="w-full h-full"
                 title="YouTube video player"
